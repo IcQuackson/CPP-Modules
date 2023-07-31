0da70af57402481878b244a1141ca539c60d6245
IcQuackson <pedrorenato1998@gmail.com>
2023-07-31 17:58:10 +0100

    Added ex01 done

---
+++ CPP module 00/ex01/includes/PhoneBook.class.hpp
@@ -0,0 +1,26 @@
+#ifndef PHONEBOOK_CLASS_HPP
+# define PHONEBOOK_CLASS_HPP
+
+#include "Contact.class.hpp"
+//# include "phonebook.hpp"
+//class Contact;
+
+
+class PhoneBook {
+
+private:
+	Contact contact_list[8];
+	int current_index;
+	int	size;
+public:
+	PhoneBook(void);
+
+	bool addContact(std::string first, std::string last,
+                 std::string nick, std::string phone,
+                 std::string secret);
+	void displayAllContacts(void);
+	void displayContact(int index);
+	int	 getSize(void);
+};
+
+#endif
---
+++ CPP module 00/ex01/includes/phonebook.hpp
@@ -0,0 +1,7 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class PhoneBook;
+class Contact;
+
+#endif
