095d4ff54109eef49caaabc01bcb07adb6c81791
IcQuackson <pedrorenato1998@gmail.com>
2023-09-01 01:36:32 +0100

    -In CPP02 EX00 Changed integerPart variable name to number for for relevance. -Added CPP02 EX01 functional and tested

--- CPP module 02/ex01/includes/Fixed.hpp
+++ CPP module 02/ex01/includes/Fixed.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
 class Fixed {
 
@@ -11,10 +12,16 @@ private:
 
 public:
 	Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
 	Fixed(const Fixed &fixed);
 	Fixed& operator= (const Fixed &fixed);
 	~Fixed();
 
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
--- CPP module 02/ex01/src/Fixed.cpp
+++ CPP module 02/ex01/src/Fixed.cpp
@@ -8,6 +8,16 @@ Fixed::Fixed(void) {
 	this->number = 0;
 }
 
+Fixed::Fixed(const int value) {
+	std::cout << "Int constructor called" << std::endl;
+	this->number = value << NUM_FRAC_BITS;
+}
+
+Fixed::Fixed(const float value) {
+	std::cout << "Float constructor called" << std::endl;
+	this->number = roundf(value * (1 << NUM_FRAC_BITS));
+}
+
 Fixed::Fixed(const Fixed &fixed) {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
@@ -20,7 +30,7 @@ Fixed::~Fixed() {
 Fixed& Fixed::operator=(const Fixed &fixed) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &fixed) {
-		this->number = fixed.getRawBits();
+		this->number = fixed.number;
 	}
 	return *this;
 }
@@ -33,4 +43,18 @@ int	Fixed::getRawBits(void) const {
 void Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->number = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return static_cast<float> (this->number) / (1 << NUM_FRAC_BITS);
+}
+
+int Fixed::toInt(void) const {
+	return this->number >> NUM_FRAC_BITS;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed)
+{
+    out << fixed.toFloat();
+    return out;
 }
