e8e2c6fda7937a9cb7c0c74dc957c299abba727d
Pedro Renato Mariano de Figueiredo Goncalves <pedgonca@c2r2s8.42lisboa.com>
2023-09-06 16:27:14 +0100

    Changed some parameter names, tested and checked for errors

--- CPP module 03/ex02/includes/ScavTrap.hpp
+++ CPP module 03/ex02/includes/ScavTrap.hpp
@@ -7,8 +7,8 @@ class ScavTrap : public ClapTrap {
 public:
 	ScavTrap();
 	ScavTrap(std::string name);
-	ScavTrap(const ScavTrap &clapTrap);
-	ScavTrap& operator= (const ScavTrap &clapTrap);
+	ScavTrap(const ScavTrap &scavTrap);
+	ScavTrap& operator= (const ScavTrap &scavTrap);
 	~ScavTrap();
 
 	void attack(const std::string& target);
