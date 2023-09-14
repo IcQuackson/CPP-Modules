b3d16e840a5f10619cd83e543729b8505c207a26
IcQuackson <pedrorenato1998@gmail.com>
2023-09-14 20:19:57 +0100

    feature: added all conversions except for special types

--- CPP module 06/ex00/includes/ScalarConverter.hpp
+++ CPP module 06/ex00/includes/ScalarConverter.hpp
@@ -3,6 +3,12 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <climits>
+
+#define INT_MIN_STR "-2147483648"
+#define INT_MAX_STR "2147483647"
+#define INT_PLUS_MAX_STR "+2147483647"
 
 class ScalarConverter {
 
@@ -10,18 +16,16 @@ private:
 	static bool isChar(std::string literal);
 	static bool isInt(std::string literal);
 	static bool isFloat(std::string literal);
-	static bool isDouble(std::string literal);
-	static int	toInt(std::string literal);
+	static bool isPseudo(std::string literal);
+	static void printCharConversion(std::string literal);
+	static std::string intToChar(int num);
 
-public:
 	ScalarConverter();
 	ScalarConverter(ScalarConverter const &other);
+
+public:
 	ScalarConverter &operator=(ScalarConverter const &other);
 	~ScalarConverter();
 	
-	static void convert(std::string literal);
-	static void printCharConversion(std::string literal);
-	static void printIntConversion(std::string literal);
-	static void printFloatConversion(std::string literal);
-	static void printDoubleConversion(std::string literal);
+	static void convert(const std::string &literal);
 };
--- CPP module 06/ex00/src/ScalarConverter.cpp
+++ CPP module 06/ex00/src/ScalarConverter.cpp
@@ -14,33 +14,100 @@ ScalarConverter::ScalarConverter(ScalarConverter const &other) {
 ScalarConverter::~ScalarConverter() {}
 
 bool ScalarConverter::isChar(std::string literal) {
-	return literal.size() == 1 && std::isprint(literal[0]) && !std::isdigit(literal[0]);
-}
-
-int ScalarConverter::toInt(std::string literal) {
-	return static_cast<int>(literal.c_str()[0]);
+	return literal.size() == 1 &&
+	((literal[0] >= 'a' && literal[0] <= 'z') ||
+	 (literal[0] >= 'A' && literal[0] <= 'Z'));
 }
 
 void ScalarConverter::printCharConversion(std::string literal) {
-	if (literal.length() == 1) {
-		char character = literal[0];
-		std::cout << "char: "; 
-		if (std::isprint(character)) {
-			std::cout << static_cast<char>(character) << std::endl;
-		}
-		else {
-			std::cout << "Non displayable" << std::endl;
-		}
-	}
+	char character = literal[0];
+	std::cout << "char: "; 
+	std::cout << static_cast<char>(character) << std::endl;
+	std::cout << "int: ";
+	std::cout << static_cast<int>(character) << std::endl;
+	std::cout << "float: ";
+	std::cout << static_cast<float>(character) << ".0f" << std::endl;
+	std::cout << "double: ";
+	std::cout << static_cast<double>(character) << ".0" << std::endl;
 }
 
-void ScalarConverter::convert(std::string literal) {
-	printCharConversion(literal);
+/* void ScalarConverter::printIntConversion(std::string literal) {
 
-	if (isChar(literal)) {
+} */
+
+bool ScalarConverter::isPseudo(std::string literal) {
+	std::string pseudoLiterals[] = {"-inf", "+inf", "-inff", "+inff", "nan", "nanf"};
+
+	for (int i = 0; i < 6; i++) {
+		if (pseudoLiterals[i] == literal) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool ScalarConverter::isFloat(std::string literal) {
+	return literal[literal.length() - 1] == 'f';
+}
+
+std::string ScalarConverter::intToChar(int num) {
+	std::string charConversion;
+
+	charConversion = static_cast<char>(num);
+	if (num < 32 || num > 126) {
+		charConversion = "Non displayable";
+	}
+	else {
+		charConversion = "'";
+		charConversion += static_cast<char>(num);
+		charConversion += "'";
+	}
+	return charConversion;
+}
+
+void ScalarConverter::convert(const std::string &literal) {
+
+	std::string charConversion = "";
+	int intConversion = 0;
+	float floatConversion = 0;
+	double doubleConversion = 0;
+
+	if ((isChar(literal))) {
+		printCharConversion(literal);
+		return;
+	}
+	intConversion = std::atoi(literal.c_str());
+
+	if (isFloat(literal)) {
+		floatConversion = std::atof(literal.c_str());
+		doubleConversion = static_cast<double>(floatConversion);
+	}
+	else {
+		doubleConversion = std::atof(literal.c_str());
+		floatConversion = static_cast<float>(doubleConversion);
+	}
+	if (isPseudo(literal)) {
+		charConversion = "impossible";
+	}
+	else {
+		charConversion = intToChar(intConversion);
+	}
+	std::cout << "char: ";
+	if (intConversion < 32 || intConversion > 126) {
+		std::cout << "Non displayable" << std::endl;
+	}
+	else {
+		std::cout << static_cast<char>(intConversion) << std::endl;
+	}
+	if (charConversion == "impossible") {
 		
 	}
-	//printIntConversion(literal);
-	//printFloatConversion(literal);
-	//printDoubleConversion(literal);
+	std::cout <<"int: " << intConversion << std::endl;
+	std::cout << "float: " << floatConversion << ".0f" << std::endl;
+	std::cout << "double: " << doubleConversion << ".0" << std::endl;
+
+	/* 
+	else {
+		std::cout << "No conversion possible" << std::endl;
+	} */
 }
